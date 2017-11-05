05d1726885671fd97d812449d8aa7e186f7b0bcf
VytautasKz <32774768+VytautasKz@users.noreply.github.com>
2017-11-05 22:18:48 +0200

    V2

--- app/src/main/java/com/example/vytas/testy/MainActivity.java
+++ app/src/main/java/com/example/vytas/testy/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vytas.testy;
 
 import android.content.pm.ActivityInfo;
 import android.os.CountDownTimer;
+import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,8 +15,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txt;
-    Button start;
+    TextView txt; /*txt kaip kreipsimes i textview*/
+    Button pass; /*kaip kreipsimes i mygtuka*/
+    Button next;
 
 
     @Override
@@ -27,20 +29,35 @@ public class MainActivity extends AppCompatActivity {
         game();
     }
 
-    public void game(){
+    public void game() {
         final String[] zodziai = {"G", "B", "C", "D"};
 
-        txt=(TextView) findViewById(R.id.textView);
-        start=(Button) findViewById(R.id.passButton);
+        txt = (TextView) findViewById(R.id.textView); /*koks txt yra id*/
+        pass = (Button) findViewById(R.id.passButton); /*koks pass yra id*/
+        next = (Button) findViewById(R.id.nextButton);
 
-        start.setOnClickListener(new View.OnClickListener(){
+
+        pass.setOnClickListener(new View.OnClickListener() { /*ziuri kada paspaudzia mygtuka*/
             @Override
-            public void onClick(View view){
-                for(int i = 0; i < zodziai.length; i++)
-                txt.setText(zodziai[i]);
+            public void onClick(View view) {
+                Random r = new Random();
+                int index = r.nextInt(zodziai.length); /*isrenka random skaiciu nuo 0 iki array ilgio*/
+                if(zodziai[index] != null) { /*nebaigta cia, noriu kad parinkus zodi jo neimtu*/
+                    txt.setText(zodziai[index]);
+
+                }else if(zodziai[index] == null){
+                    txt.setText("Out of words");
+                }
             }
         });
-
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random r = new Random();
+                int index1 = r.nextInt(zodziai.length);
+                txt.setText(zodziai[index1]);
+            }
+        });
     }
 }
 
